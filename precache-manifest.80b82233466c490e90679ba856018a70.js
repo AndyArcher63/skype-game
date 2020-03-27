a77bdbba0fc237d381418b454e5cc23c50961b42
Andy Archer <andy.archer@zonedigital.com>
2020-03-27 13:10:07 +0000

    Updates

---
+++ precache-manifest.80b82233466c490e90679ba856018a70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "333089e9cb5861605d8b9214fcaae58b",
+    "url": "/skype-game/index.html"
+  },
+  {
+    "revision": "00d1a15282b045add112",
+    "url": "/skype-game/static/css/main.1e4fd6bf.chunk.css"
+  },
+  {
+    "revision": "6c11287278286d703ddc",
+    "url": "/skype-game/static/js/2.3a15c2e1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/skype-game/static/js/2.3a15c2e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00d1a15282b045add112",
+    "url": "/skype-game/static/js/main.5ed39487.chunk.js"
+  },
+  {
+    "revision": "51405780cb712e949c72",
+    "url": "/skype-game/static/js/runtime-main.307a5069.js"
+  }
+]);
